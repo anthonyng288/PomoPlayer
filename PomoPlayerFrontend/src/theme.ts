b4befde2f122027f07c3c1f9b29d9c58c55db898
anthonyng288 <anthonynguyen832@gmail.com>
2025-02-10 11:09:22 +1000

    Override MUI Themes because I want specific coloured borders
    Signed-off-by: anthonyng288 <anthonynguyen832@gmail.com>

--- PomoPlayerFrontend/src/theme.ts
+++ PomoPlayerFrontend/src/theme.ts
@@ -1,5 +1,5 @@
 import { createTheme } from "@mui/material";
-
+import { outlinedInputClasses } from "@mui/material/OutlinedInput";
 import { red } from "@mui/material/colors";
 
 const theme = createTheme({
@@ -13,6 +13,32 @@ const theme = createTheme({
   },
   typography: {
     fontFamily: "Kanit, sans-serif",
+  },
+  components: {
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          "--TextField-brandBorderColor": "#B2BAC2",
+          "--TextField-brandBorderHoverColor": "#FFFF",
+          "--TextField-brandBorderFocusedColor": "#6F7E8C",
+        },
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        notchedOutline: {
+          borderColor: "var(--TextField-brandBorderColor)",
+        },
+        root: {
+          [`&:hover .${outlinedInputClasses.notchedOutline}`]: {
+            borderColor: "var(--TextField-brandBorderHoverColor)",
+          },
+          [`&.Mui-focused .${outlinedInputClasses.notchedOutline}`]: {
+            borderColor: "var(--TextField-brandBorderFocusedColor)",
+          },
+        },
+      },
+    },
   },
 });
 
